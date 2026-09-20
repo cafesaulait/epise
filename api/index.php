a78bce73ff83d5ffc4a676a2fcc352b4e2ccd890
Angelina CHADES <a.chades14@gmail.com>
2026-09-20 21:21:47 +1100

    Add CORS headers to index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,9 @@
 <?php
+
+header("Access-Control-Allow-Origin: https://epise-unc.netlify.app/");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 error_reporting(E_ALL);
 ini_set('display_errors', 0);
 
